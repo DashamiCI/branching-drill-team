b5920be6741f99b4f02544b88a1f260927eb4d16
Om_Chaudhari <om.chaudhari@cloverinfotech.com>
2026-06-25 16:42:42 +0530

    Added logging feature

--- calc.py
+++ calc.py
@@ -1,23 +1,29 @@
+import logging
+
 def add(a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("Inputs must be numeric")
+    logging.warning(f"add: {a}, {b}")
     return a + b
 
 
 def subtract(a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("Inputs must be numeric")
+    logging.warning(f"subtract: {a}, {b}")
     return a - b
 
 
 def multiply(a, b):
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("Inputs must be numeric")
+    logging.warning(f"multiply: {a}, {b}")
     return a * b
 
 def divide(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
+    logging.warning(f"divide: {a}, {b}")
     return a / b
 
 if __name__ == "__main__":
