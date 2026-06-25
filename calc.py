d40746bfa9ab81e177d51be05e58aa887bb6b306
Dashami Ghosalkar <dashami.ghosalkar@cloverinfotech.com>
2026-06-25 16:01:28 +0530

    added calc.py

---
+++ calc.py
@@ -0,0 +1,19 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+
+    print("Addition:", add(x, y))
+    print("Subtraction:", subtract(x, y))
+    print("Multiplication:", multiply(x, y))
