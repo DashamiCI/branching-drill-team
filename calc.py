d579018d6368aa325376bdbbda87ace9822a1b23
Om_Chaudhari <om.chaudhari@cloverinfotech.com>
2026-06-25 16:34:10 +0530

    Revert "Merge pull request #1 from DashamiCI/feature/add-logging"
    This reverts commit a90bf21291eb3bc97e4bd3df7368f65c16c1981c, reversing
    changes made to d40746bfa9ab81e177d51be05e58aa887bb6b306.

--- calc.py
+++ calc.py
@@ -1,18 +1,19 @@
-import logging
-
 def add(a, b):
-    logging.warning(f"add: {a}, {b}")
     return a + b
 
+
 def subtract(a, b):
-    logging.warning(f"subtract: {a}, {b}")
     return a - b
 
+
 def multiply(a, b):
-    logging.warning(f"multiply: {a}, {b}")
     return a * b
 
+
 if __name__ == "__main__":
-    add(10, 5)
-    subtract(10, 5)
-    multiply(10, 5)
+    x = 10
+    y = 5
+
+    print("Addition:", add(x, y))
+    print("Subtraction:", subtract(x, y))
+    print("Multiplication:", multiply(x, y))
